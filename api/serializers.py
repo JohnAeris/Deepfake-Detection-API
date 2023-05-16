bb6f3af4b11a91f5dc055a5d1de30de7ad2b98e8
Romeo Encinares <romeoncrs@gmail.com>
2023-05-16 20:54:03 +0800

    Add class meta in VideoSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
-from base.models import Item
+from base.models import Item, Video
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__' 
 
-class VideoSerializer(serializers.Serializer):
+class VideoSerializer(serializers.ModelSerializer):
     video = serializers.FileField()
+
+    class Meta:
+        model = Video
+        fields = '__all__' 
