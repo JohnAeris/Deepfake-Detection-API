dbaea023e599af24134a11f62acfd903cad92461
Romeo Encinares <romeoncrs@gmail.com>
2023-05-16 15:49:45 +0800

    Add addVideo API endpoint

--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Item
-from .serializers import ItemSerializer
+from .serializers import ItemSerializer, VideoSerializer
+
 
 @api_view(['GET'])
 def getData(request):
@@ -14,4 +15,11 @@ def addItem(request):
     serializer = ItemSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addVideo(request):
+    serializer = VideoSerializer(data=request.data)
+    if serializer.is_valid():
+        print(request.data)
     return Response(serializer.data)
