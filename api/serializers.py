92a8d9856cb36d33d85fddbd0185a3709ebdca91
Romeo Encinares <romeoncrs@gmail.com>
2023-05-16 15:49:17 +0800

    Create VideoSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -5,3 +5,6 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__' 
+
+class VideoSerializer(serializers.Serializer):
+    video = serializers.FileField()
