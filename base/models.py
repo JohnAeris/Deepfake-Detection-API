39e3d06390fd5102c1b1289f91a83656d4a33ee5
Romeo Encinares <romeoncrs@gmail.com>
2023-05-16 20:53:17 +0800

    Add video model

--- base/models.py
+++ base/models.py
@@ -4,3 +4,8 @@ from django.db import models
 class Item(models.Model):
     name = models.CharField(max_length=200)
     created = models.DateTimeField(auto_now_add=True)
+
+# Create your models here.
+class Video(models.Model):
+    video = models.FileField()
+    created = models.DateTimeField(auto_now_add=True)
